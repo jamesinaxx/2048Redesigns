a1746aab7e3136f4cfeed3abcd2c9a251b73164f
jamesafk <grassojames5@gmail.com>
2020-11-25 09:22:22 +1100

    Added everything to the main.js file

---
+++ scripts/chrome/main.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function () {
+    console.log("Extension installed and activated!");
+});
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostContains: ''},
+        })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+});

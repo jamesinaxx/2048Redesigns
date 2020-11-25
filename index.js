dc8ecd5b5406878836627900dd43ee6950a200c4
jamesafk <grassojames5@gmail.com>
2020-11-26 09:57:13 +1100

    Saved the file

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const {app, BrowserWindow} = require('electron');
   const path = require('path');
   const url = require('url');
 
-const DiscordRPC = require('discord-rpc');
-
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-const startTimestamp = new Date();
-
 const clientId = '778575775104106496';
 
   app.on('ready', () => {
@@ -22,37 +17,9 @@ const clientId = '778575775104106496';
     win.on('ready-to-show', () => {
       win.show();
       win.maximize();
-
-      rpc.on('ready', () => {
-        setActivity(rpc);
-
-        setInterval(() => {
-          setActivity(rpc);
-        }, 5000);
-      });
     });
-    rpc.login({ clientId }).catch(console.error);
   });
 
   app.on('window-all-closed', () => {
     app.quit();
   });
-  
-  function setActivity(rpc) {
-    console.info(currentGame);
-
-    if (!rpc) {
-      return;
-    };
-
-    rpc.setActivity({
-      details: `The Rainbow cupcake is desired by all`,
-      state: 'Joining cupcakes!',
-      startTimestamp: startTimestamp,
-      largeImageKey: currentGame,
-      largeImageText: currentGame,
-      smallImageKey: "jamesafk",
-      smallImageText: 'Shamless plug of me :)',
-      instance: false,
-    });
-  };

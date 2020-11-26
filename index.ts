f2b78f22ac0b9ebefe8ae312d47be22aa9b42611
James Grasso <grassojames5@gmail.com>
2020-11-26 17:46:10 +1100

    Organized more stuff

--- index.ts
+++ index.ts
@@ -4,22 +4,22 @@ const {app, BrowserWindow} = require('electron');
 
 const clientId = '778575775104106496';
 
-  app.on('ready', () => {
+app.on('ready', () => {
 
-    const win = new BrowserWindow({ width: 800, height: 800, show: false, autoHideMenuBar: true, icon: './assets/icons/favicon.ico' });
+  const win = new BrowserWindow({ width: 800, height: 800, show: false, autoHideMenuBar: true, icon: './assets/icons/favicon.ico' });
 
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 
-    win.on('ready-to-show', () => {
-      win.show();
-      win.maximize();
-    });
+  win.on('ready-to-show', () => {
+    win.show();
+    win.maximize();
   });
+});
 
-  app.on('window-all-closed', () => {
-    app.quit();
-  });
+app.on('window-all-closed', () => {
+  app.quit();
+});
